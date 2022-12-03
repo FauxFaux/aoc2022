d16e6de9e5b40cc1477d4e741051d00cc9c42744
Chris West (Faux) <git@goeswhere.com>
2022-12-03 18:49:44 +0000

    d3p2

--- src/d3.rs
+++ src/d3.rs
@@ -5,11 +5,18 @@ pub fn solve() {
     let pairs = include_str!("d3.txt")
         .trim()
         .lines()
-        .map(|x| x.split_at(x.len() / 2))
-        .map(|(l, r)| {
+        .tuples()
+        .map(|(l, r, b)| {
             let l: HashSet<char> = l.chars().collect();
             let r: HashSet<char> = r.chars().collect();
-            let d = l.intersection(&r).copied().collect_vec();
+            let b: HashSet<char> = b.chars().collect();
+            let d = l
+                .intersection(&r)
+                .copied()
+                .collect::<HashSet<_>>()
+                .intersection(&b)
+                .copied()
+                .collect_vec();
             assert_eq!(1, d.len());
             d[0] as u8
         })
