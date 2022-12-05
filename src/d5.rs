b5eaa8882ab294532a94745d3ce215808e3021ca
Chris West (Faux) <git@goeswhere.com>
2022-12-05 09:19:40 +0000

    d5p1

---
+++ src/d5.rs
@@ -0,0 +1,58 @@
+use itertools::Itertools;
+
+pub fn solve() {
+    let lines = include_str!("d5.txt").lines().collect_vec();
+
+    let (grid, moves) = lines.split_at(lines.iter().position(|x| x.is_empty()).unwrap());
+    let mut grid = grid
+        .into_iter()
+        .map(|l| {
+            format!("{l} ")
+                .chars()
+                .tuples()
+                .map(|(_o, c, _c, _s)| c)
+                .collect_vec()
+        })
+        .collect_vec();
+    let _key = grid.pop().unwrap();
+    let width = grid.iter().map(|x| x.len()).max().unwrap();
+    let mut g = (0..width).map(|_| Vec::<char>::new()).collect_vec();
+    for line in grid.iter().rev() {
+        for (i, c) in line.into_iter().enumerate() {
+            if c.is_ascii_whitespace() {
+                continue;
+            }
+            g[i].push(*c);
+        }
+    }
+
+    let moves = moves
+        .into_iter()
+        .skip(1) // split point
+        .map(|line| {
+            line.split_whitespace()
+                .tuples()
+                .map(|(_m, c, _f, s, _t, d)| (p(c), p(s) - 1, p(d) - 1))
+                .next()
+                .unwrap()
+        })
+        .collect_vec();
+
+    for (count, source, dest) in moves.into_iter() {
+        for _ in 0..count {
+            let item = g[source].pop().unwrap();
+            g[dest].push(item);
+        }
+    }
+
+    println!("{g:?}");
+
+    println!(
+        "{}",
+        g.into_iter().map(|x| x[x.len() - 1]).collect::<String>()
+    );
+}
+
+fn p(s: &str) -> usize {
+    s.parse().unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ mod d1;
 mod d2;
 mod d3;
 mod d4;
+mod d5;
 
 fn main() {
-    match 4 {
+    match 5 {
         1 => d1::solve(),
         2 => d2::solve(),
         3 => d3::solve(),
         4 => d4::solve(),
+        5 => d5::solve(),
         _ => unreachable!(),
     }
 }
