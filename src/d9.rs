0c76f7c141d5fc3a8b4a3fdffd754571636c8c75
Chris West (Faux) <git@goeswhere.com>
2022-12-09 08:54:01 +0000

    d9p2

--- src/d9.rs
+++ src/d9.rs
@@ -14,7 +14,7 @@ pub fn solve() {
 
     let mut visited = HashSet::<Coord>::new();
     let mut head: Coord = (0, 0);
-    let mut tail: Coord = (0, 0);
+    let mut tails: Vec<Coord> = vec![(0, 0); 9];
     for cmd in cmds {
         match cmd {
             'L' => head.0 -= 1,
@@ -23,29 +23,35 @@ pub fn solve() {
             'D' => head.1 += 1,
             other => unreachable!("move: {other:?}"),
         }
-        let pick = match (head.0 - tail.0, head.1 - tail.1) {
-            (x, y) if x.abs() <= 1 && y.abs() <= 1 => (0, 0),
-            (0, y) if y > 0 => (0, 1),
-            (0, y) if y < 0 => (0, -1),
-            (x, 0) if x < 0 => (-1, 0),
-            (x, 0) if x > 0 => (1, 0),
-            (x, y) if x > 0 && y > 0 => (1, 1),
-            (x, y) if x < 0 && y < 0 => (-1, -1),
-            (x, y) if x > 0 && y < 0 => (1, -1),
-            (x, y) if x < 0 && y > 0 => (-1, 1),
-            other => unreachable!("tail: {other:?}"),
-        };
+        mv(head, &mut tails[0]);
+        for (i, (targ, tail)) in (0..9).tuple_windows().enumerate() {
+            mv(tails[targ], &mut tails[tail]);
+            println!("{cmd} {i} targ: {targ:?}, tail: {tail:?}");
+        }
 
-        println!("{cmd} head: {head:?}, tail: {tail:?}, pick: {pick:?}");
-
-        tail.0 += pick.0;
-        tail.1 += pick.1;
-
-        visited.insert(tail);
+        visited.insert(*tails.last().unwrap());
     }
 
     println!("{visited:?}");
     println!("{}", visited.len());
+}
+
+fn mv(targ: Coord, tail: &mut Coord) {
+    let pick = match (targ.0 - tail.0, targ.1 - tail.1) {
+        (x, y) if x.abs() <= 1 && y.abs() <= 1 => (0, 0),
+        (0, y) if y > 0 => (0, 1),
+        (0, y) if y < 0 => (0, -1),
+        (x, 0) if x < 0 => (-1, 0),
+        (x, 0) if x > 0 => (1, 0),
+        (x, y) if x > 0 && y > 0 => (1, 1),
+        (x, y) if x < 0 && y < 0 => (-1, -1),
+        (x, y) if x > 0 && y < 0 => (1, -1),
+        (x, y) if x < 0 && y > 0 => (-1, 1),
+        other => unreachable!("tail: {other:?}"),
+    };
+
+    tail.0 += pick.0;
+    tail.1 += pick.1;
 }
 
 fn p(s: &str) -> usize {
