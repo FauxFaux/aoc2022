97f681c190cc1090008fec4a8c68e49884f3155f
Chris West (Faux) <git@goeswhere.com>
2022-12-11 11:44:40 +0000

    d11p1

--- src/d11.rs
+++ src/d11.rs
@@ -15,12 +15,15 @@ pub fn solve() {
         })
         .collect_vec();
 
+    let mut inspections = vec![0usize; monk.len()];
     let mut curs: Vec<Vec<i64>> = monk.iter().map(|(s, _, _, _, _)| s.clone()).collect_vec();
     for round in 0..20 {
-        let mut new: Vec<Vec<i64>> = vec![Vec::new(); monk.len()];
-        for (mn, cur) in curs.clone().into_iter().enumerate() {
+        for mn in 0..curs.len() {
             let (_s, op, test, tru, fals) = &monk[mn];
-            for mut item in cur {
+            let this_monk = curs[mn].clone();
+            curs[mn].clear();
+            for mut item in this_monk {
+                inspections[mn] += 1;
                 let t = |s| match s {
                     "old" => item,
                     o => p(o),
@@ -39,14 +42,23 @@ pub fn solve() {
 
                 let next = *if item % test == 0 { tru } else { fals };
                 println!("round: {round} monkey: {mn} item new value: {item} thrown to: {next}");
-                new[next].push(item);
+                curs[next].push(item);
             }
         }
-        curs = new;
         for (mo, holding) in curs.iter().enumerate() {
             println!("{round} | {mo} | {holding:?}");
         }
     }
+    println!("{:#?}", inspections);
+    println!(
+        "{:#?}",
+        inspections
+            .into_iter()
+            .sorted()
+            .rev()
+            .take(2)
+            .product::<usize>()
+    );
 }
 
 fn sr<'s>(s: &'s str, needle: &str) -> &'s str {
