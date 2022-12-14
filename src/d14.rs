823d92f5be0b2bf99a05f782355dfb7f7044a218
Chris West (Faux) <git@goeswhere.com>
2022-12-14 12:03:11 +0000

    bitset

--- src/d14.rs
+++ src/d14.rs
@@ -1,18 +1,43 @@
 use itertools::Itertools;
-use std::collections::HashSet;
+use num_integer::div_rem;
+use std::fmt::{Debug, Formatter};
 
-type Pos = (i16, i16);
+#[derive(Copy, Clone)]
+struct Pos(usize);
+
+const W: usize = 1024;
+
+impl Pos {
+    fn new((x, y): (i16, i16)) -> Self {
+        Pos(y as usize * W + x as usize)
+    }
+
+    fn adj(self) -> [Pos; 3] {
+        [Pos(self.0 + W), Pos(self.0 + W - 1), Pos(self.0 + W + 1)]
+    }
+
+    fn xy(self) -> (i16, i16) {
+        let (y, x) = div_rem(self.0, W);
+        (x as i16, y as i16)
+    }
+}
+
+impl Debug for Pos {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self.xy())
+    }
+}
 
 pub fn solve() {
     let lines = include_str!("d14.txt")
         .lines()
-        .map(|x| x.split(" -> ").map(p).collect_vec())
+        .map(|x| x.split(" -> ").map(t).collect_vec())
         .collect_vec();
 
-    let mut grid = HashSet::<Pos>::with_capacity(900_000);
+    let mut grid = bit_set::BitSet::with_capacity(900000);
     for line in &lines {
         let mut cursor = line[0];
-        grid.insert(cursor);
+        grid.insert(Pos::new(cursor).0);
         for coord in line {
             loop {
                 match (cursor.0 - coord.0, cursor.1 - coord.1) {
@@ -25,34 +50,33 @@ pub fn solve() {
                     }
                     (_, _) => unreachable!("diagonal lines {cursor:?} {coord:?}"),
                 }
-                grid.insert(cursor);
+                grid.insert(Pos::new(cursor).0);
             }
         }
     }
 
-    let bottom = 1000 + *grid.iter().map(|(_, y)| y).max().unwrap();
+    let bottom = grid.iter().map(|p| Pos(p).xy().1).max().unwrap() as usize;
+    let bottom_inner = Pos::new((0, (1 + bottom) as i16)).0;
     let mut rested = 0usize;
 
-    while !grid.contains(&(500, 0)) {
-        let mut sand: Pos = (500, 0);
+    let start = Pos::new((500, 0));
+
+    while !grid.contains(start.0) {
+        let mut sand: Pos = start;
         'sand: loop {
-            if sand.1 == bottom {
-                grid.insert(sand);
+            if sand.0 > bottom_inner {
+                grid.insert(sand.0);
                 rested += 1;
                 break 'sand;
             }
-            for cand in [
-                (sand.0, sand.1 + 1),
-                (sand.0 - 1, sand.1 + 1),
-                (sand.0 + 1, sand.1 + 1),
-            ] {
-                if !grid.contains(&cand) {
+            for cand in sand.adj() {
+                if !grid.contains(cand.0) {
                     // println!("{sand:?} -> {cand:?}");
                     sand = cand;
                     continue 'sand;
                 }
             }
-            grid.insert(sand);
+            grid.insert(sand.0);
             rested += 1;
             break;
         }
@@ -61,7 +85,7 @@ pub fn solve() {
     println!("{rested}");
 }
 
-fn p(s: &str) -> Pos {
+fn t(s: &str) -> (i16, i16) {
     let (a, b) = s.split_once(',').unwrap();
     (i(a), i(b))
 }
