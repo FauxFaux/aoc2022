256deba598d974c157e9debb3a22ff74f72b2df1
Chris West (Faux) <git@goeswhere.com>
2022-12-18 10:52:39 +0000

    d16: switch to numeric key

--- src/d16.rs
+++ src/d16.rs
@@ -1,5 +1,33 @@
 use itertools::Itertools;
 use std::collections::HashMap;
+use std::fmt::{Debug, Display, Formatter};
+
+#[derive(Copy, Clone, Hash, Eq, PartialEq, Ord, PartialOrd)]
+struct Key(u16);
+
+impl Key {
+    fn new(s: &str) -> Key {
+        let [a, b]: [u8; 2] = s.bytes().collect_vec().try_into().unwrap();
+        Key((a as u16) * 256 + b as u16)
+    }
+}
+
+impl Display for Key {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}{}",
+            (self.0 / 256) as u8 as char,
+            self.0 as u8 as char
+        )
+    }
+}
+
+impl Debug for Key {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
 
 pub fn solve() {
     let graph = include_str!("d16.txt")
@@ -8,18 +36,18 @@ pub fn solve() {
             let x = x
                 .split(|c| matches!(c, ';' | '=' | ' ' | ','))
                 .collect_vec();
-            let v = x[1];
+            let v = Key::new(x[1]);
             let r = p(x[5]);
             let t = x[11..]
                 .iter()
                 .filter(|x| !x.is_empty())
                 // stupid ass-language
-                .map(|x| *x)
+                .map(|x| Key::new(x))
                 .collect_vec()
                 .into_boxed_slice();
             (v, (r, t))
         })
-        .collect::<HashMap<&str, (usize, _)>>();
+        .collect::<HashMap<Key, (usize, _)>>();
 
     let mut interesting = graph
         .iter()
@@ -27,29 +55,34 @@ pub fn solve() {
         .map(|(dest, _)| *dest)
         .collect_vec();
 
-    interesting.insert(0, "AA");
+    interesting.insert(0, Key::new("AA"));
+
+    let mut derived = HashMap::with_capacity(interesting.len());
+
     for from in &interesting {
         for to in &interesting {
             if from == to {
                 continue;
             }
-            let dist = find(&graph, from, to, 0);
-            println!("{from} {to} {dist}");
+            let dist = find(&graph, *from, *to, 0);
+            derived.entry(from).or_insert(Vec::new()).push((to, dist));
         }
     }
+    println!("{derived:?}");
 
     let mut memo = HashMap::with_capacity(9000);
-    let best = search(&graph, &mut memo, "AA", 0, HashMap::with_capacity(8));
+    let best = search(
+        &graph,
+        &mut memo,
+        Key::new("AA"),
+        0,
+        HashMap::with_capacity(8),
+    );
 
     println!("{best:?}")
 }
 
-fn find(
-    grid: &HashMap<&'static str, (usize, Box<[&'static str]>)>,
-    from: &str,
-    to: &str,
-    dist: usize,
-) -> usize {
+fn find(grid: &HashMap<Key, (usize, Box<[Key]>)>, from: Key, to: Key, dist: usize) -> usize {
     if dist > 13 {
         return dist;
     }
@@ -58,23 +91,23 @@ fn find(
     }
 
     let mut guesses = Vec::with_capacity(8);
-    for cand in grid[from].1.iter() {
-        guesses.push(find(grid, cand, to, dist + 1));
+    for cand in grid[&from].1.iter() {
+        guesses.push(find(grid, *cand, to, dist + 1));
     }
 
     guesses.into_iter().min().unwrap_or(30)
 }
 
-fn flatten(on: &HashMap<&'static str, u8>) -> Vec<(&'static str, u8)> {
+fn flatten(on: &HashMap<Key, u8>) -> Vec<(Key, u8)> {
     on.iter().map(|(x, y)| (*x, *y)).sorted().collect()
 }
 
 fn search(
-    grid: &HashMap<&'static str, (usize, Box<[&'static str]>)>,
-    memo: &mut HashMap<(&'static str, u8, Vec<(&'static str, u8)>), usize>,
-    here: &'static str,
+    grid: &HashMap<Key, (usize, Box<[Key]>)>,
+    memo: &mut HashMap<(Key, u8, Vec<(Key, u8)>), usize>,
+    here: Key,
     minute: u8,
-    on: HashMap<&'static str, u8>,
+    on: HashMap<Key, u8>,
 ) -> usize {
     if let Some(existing) = memo.get(&(here, minute, flatten(&on))) {
         return *existing;
@@ -91,8 +124,8 @@ fn search(
 
     let mut opts = Vec::with_capacity(6);
 
-    let (v, neigh) = &grid[here];
-    if *v != 0 && !on.contains_key(here) {
+    let (v, neigh) = &grid[&here];
+    if *v != 0 && !on.contains_key(&here) {
         let mut on = on.clone();
         on.insert(here, minute);
         let key = flatten(&on);
@@ -102,7 +135,7 @@ fn search(
     }
 
     for neigh in neigh.iter() {
-        let score = search(grid, memo, neigh, minute + 1, on.clone());
+        let score = search(grid, memo, *neigh, minute + 1, on.clone());
         // memo.insert((here, minute, flatten(&on)), score);
         // println!("{minute} {here} -> {neigh} = {score}");
         opts.push(score);
